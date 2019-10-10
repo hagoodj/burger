fa4f454d050c17ca85b73b501f4c0526815b3997
jordan hagood <jordan.hagood@yahoo.com>
2019-10-09 19:22:03 -0700

    selectAll method

--- config/orm.js
+++ config/orm.js
@@ -29,3 +29,18 @@ function objToSql(ob) {
 
     return arr.toString();
 }
+
+var orm = {
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+
+  };
+
+  module.exports = orm;
