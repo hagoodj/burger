0a93ea3d10eef8ba543c0e3e1b6784a10e097bd4
jordan hagood <jordan.hagood@yahoo.com>
2019-10-09 20:05:42 -0700

    listen on port

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 var express = require("express");
 var bodyparser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/json" }));
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
