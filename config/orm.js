e4a8c0294bc84a0731e6e874183174548909402d
jordan hagood <jordan.hagood@yahoo.com>
2019-10-09 19:12:42 -0700

    helper function for sql syntax

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,12 @@
 var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
